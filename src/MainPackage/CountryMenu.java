fe7d2eb0f8a68135d8e8d3bcf89297043953f098
Atanas Velev <velevatanas@yahoo.com>
2018-05-16 23:58:13 +0300

    Weiter

---
+++ src/MainPackage/CountryMenu.java
@@ -0,0 +1,14 @@
+package MainPackage;
+
+public class CountryMenu extends Menu {
+	private String words;
+	
+	
+
+	@Override
+	protected void printMenu() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
